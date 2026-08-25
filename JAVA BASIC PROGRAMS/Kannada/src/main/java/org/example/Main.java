7e8180fd9aa89405d648a36ae66f2885fc94265b
ak17-coder <anirudhkulkarni017@gmail.com>
2026-08-25 20:05:45 +0530

    Add files via upload

---
+++ JAVA BASIC PROGRAMS/Kannada/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 0xC80; i <= 0xCFF; i++){
+            System.out.print((char)i+" ");
+        }
+    }
+}
